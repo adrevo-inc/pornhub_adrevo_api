2f3a3f76f72e80510d05bdc03e81269ccdb033a8
tsukasa.ohashi <g.high.stance@gmail.com>
2020-07-25 11:56:43 +0900

    Merge branch 'save_scraped_data'

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'top#index'
 
+  get 'scrape/get_video_keys/:channel' => 'scrape#get_video_keys'
+  get 'scrape/get_video_list'          => 'scrape#get_video_list'
+
+  get 'scrape/list_channels' => 'scrape#list_channels'
+  get 'scrape/list_videos'   => 'scrape#list_videos'
+
   get 'scrape/:channel/:label' => 'scrape#index'
-  get 'scrape/get_video_list' => 'scrape#get_video_list'
+
 
   get 'sct' => 'etc#sct'
 
