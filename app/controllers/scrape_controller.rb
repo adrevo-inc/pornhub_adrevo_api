940a1d7d9651cfd34dc2a7091d3351a0d34a11f5
tsukasa.ohashi <g.high.stance@gmail.com>
2020-08-23 16:25:03 +0900

    changed getting daily data function from table view model to rails process model

--- app/controllers/scrape_controller.rb
+++ app/controllers/scrape_controller.rb
@@ -70,14 +70,34 @@ class ScrapeController < ApplicationController
 
 ## Select DB methods
 
+  # 1日に複数回データが入っている可能性がある
+  # また、Scraping処理が伸びて24時以降に取得している可能性もある
+  # これらを考慮し、指定当日23:00〜翌日22:59までで最古のデータを取得している
+  def _filter_oldest(date, rawdata, uniq_key)
+    result = []
+    checklist = {}
+    rawdata.each do |r|
+      k = r[uniq_key]
+      if !checklist.has_key?(k) || checklist[k] > r.created_at then
+        result = (result << r)
+        checklist[k] = r.created_at
+      end
+    end
+    return result
+  end
+
   def select_channels
     logger.debug params
     date = params[:date] || Time.zone.now.strftime('%Y-%m-%d')
     channel = params[:channel]
 
+
     if channel.present? then
-#      @channels = DailyChannel.where("convert_tz(created_at,'+00:00','+09:00') = ? and channel = ?", date, channel)
-      @channels = DailyChannel.where("created_date = ? and channel = ?", date, channel)
+      datefrom = date + ' 23:00:00'
+      dateto = Time.zone.parse(datefrom).since(1.days).strftime('%Y-%m-%d %H:%M:%S')
+      rawdata = Channel.where("channel = ? and created_at >= ? and created_at < ?", channel, datefrom, dateto)
+      @channels = _filter_oldest(date, rawdata, 'channel')
+
       respond_to do |format|
         format.html { render template: 'scrape/list_channels' }
         format.json { render json: @channels }
@@ -88,11 +108,16 @@ class ScrapeController < ApplicationController
   def select_videos
     logger.debug params
     date = params[:date] || Time.zone.now.strftime('%Y-%m-%d')
+    vtype = params[:vtype] || 'premium'
     channel = params[:channel]
 
     if channel.present? then
-#      @videos =  DailyVideo.where("convert_tz(created_at,'+00:00','+09:00') = ? and channel = ?", date, channel)
-      @videos =  DailyVideo.where("created_date = ? and channel = ?", date, channel)
+      vkeys = VideoChannelMap.where("channel = ? and video_type = ?", channel, vtype).map{ |r| r.video_id }
+
+      datefrom = date + ' 23:00:00'
+      dateto = Time.zone.parse(datefrom).since(1.days).strftime('%Y-%m-%d %H:%M:%S')
+      rawdata = Video.where("video_id in (?) and created_at >= ? and created_at < ?", vkeys, datefrom, dateto)
+      @videos = _filter_oldest(date, rawdata, 'video_id')
 
       respond_to do |format|
         format.html { render template: 'scrape/list_videos' }
